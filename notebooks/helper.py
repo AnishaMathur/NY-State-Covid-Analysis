ffe653c1b08281071de6fedf4bd2c3dca8656251
Gonick Nalwa <gonick@locus.sh>
2022-04-01 19:07:01 -0700

    XGBoost

--- notebooks/helper.py
+++ notebooks/helper.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import sklearn.metrics as metrics
+import numpy as np
 
 
 def plot_feature_importance(model, title="Feature importances using MDI"):
@@ -8,8 +10,28 @@ def plot_feature_importance(model, title="Feature importances using MDI"):
     else:
         features = model.feature_importances_
 
-    fig, ax = plt.subplots(figsize=(15, 4))
+    fig, ax = plt.subplots(figsize=(15, 8))
     ax.bar(model.feature_names_in_, features)
     ax.set_title(title)
+    ax.set_xticklabels(model.feature_names_in_, rotation=90)
     ax.set_ylabel("Mean decrease in impurity")
     fig.tight_layout()
+
+
+def top_k_hsp_predction_loss(model, subset_test, target, topK):
+    unique_hsps = subset_test.index.get_level_values('Facility Name').unique()
+    losses = []
+    for analyse_hsp in unique_hsps:
+        test_set = subset_test[subset_test.index.get_level_values(
+            'Facility Name') == analyse_hsp]
+        test_X = test_set.drop(target, axis=1)
+
+        y_preds = model.predict(test_X)
+        y_test = test_set[[target]]
+
+        loss = metrics.mean_squared_error(y_test, y_preds)
+        losses.append(loss)
+    losses = np.asarray(losses)
+    ind = np.argpartition(losses, -topK)[-topK:]
+    print('Loss Min: ', losses.min(), ' | Loss Max: ', losses.max())
+    return [(unique_hsps[idx], losses[idx]) for idx in ind[np.argsort(losses[ind])]]
